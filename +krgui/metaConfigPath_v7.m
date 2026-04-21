47415226a79919326b504dd164e486986ad9c438
giftedlattice <jsayles09@gmail.com>
2026-04-21 12:46:54 -0400

    Meta Data Save
    Save location for meta data

--- +krgui/metaConfigPath_v7.m
+++ +krgui/metaConfigPath_v7.m
@@ -1,9 +1,14 @@
+% function cfgPath = metaConfigPath_v7()
+% %METACONFIGPATH_V7 Returns a stable file path for storing last-used meta.
+% try
+%     base = prefdir; % user-specific, persistent
+% catch
+%     base = pwd;
+% end
+% cfgPath = fullfile(base, 'KR_callDetectTool_v7_lastMeta.mat');
+% end
+
 function cfgPath = metaConfigPath_v7()
 %METACONFIGPATH_V7 Returns a stable file path for storing last-used meta.
-try
-    base = prefdir; % user-specific, persistent
-catch
-    base = pwd;
-end
-cfgPath = fullfile(base, 'KR_callDetectTool_v7_lastMeta.mat');
+cfgPath = fullfile(fileparts(mfilename('fullpath')), 'KR_callDetectTool_v7_lastMeta.mat');
 end
